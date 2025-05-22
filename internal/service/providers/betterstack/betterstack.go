8f333313e819bed94f5832cad8a116b0f0de57ca
Richard Kettelerij <richard@mindloops.nl>
2025-05-22 10:40:37 +0200

    feat: add BetterStack uptime provider - improve error messages

--- internal/service/providers/betterstack/betterstack.go
+++ internal/service/providers/betterstack/betterstack.go
@@ -44,28 +44,30 @@ func New(settings Settings) *BetterStack {
 func (b *BetterStack) CreateOrUpdateCheck(ctx context.Context, check model.UptimeCheck) (err error) {
 	existingCheckID, err := b.findCheck(check)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to find check %s, error: %w", check.ID, err)
 	}
 	if existingCheckID == p.CheckNotFound { //nolint:nestif // clean enough
 		log.FromContext(ctx).Info("creating check", "check", check)
 		monitorID, err := b.client.createMonitor(check)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to create monitor for check %s, error: %w", check.ID, err)
 		}
 		if err = b.client.createMetadata(check.ID, monitorID, check.Tags); err != nil {
-			return err
+			return fmt.Errorf("failed to create metadata for check %s, error: %w", check.ID, err)
 		}
 	} else {
 		log.FromContext(ctx).Info("updating check", "check", check, "betterstack ID", existingCheckID)
 		existingMonitor, err := b.client.GetMonitor(existingCheckID)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to get monitor for check %s, error: %w", check.ID, err)
 		}
 		if err = b.client.updateMonitor(check, existingMonitor); err != nil {
-			return err
+			return fmt.Errorf("failed to update monitor for check %s (betterstack ID: %d), "+
+				"error: %w", check.ID, existingCheckID, err)
 		}
 		if err = b.client.updateMetadata(check.ID, existingCheckID, check.Tags); err != nil {
-			return err
+			return fmt.Errorf("failed to update metdata for check %s (betterstack ID: %d), "+
+				"error: %w", check.ID, existingCheckID, err)
 		}
 	}
 	return err
@@ -77,17 +79,19 @@ func (b *BetterStack) DeleteCheck(ctx context.Context, check model.UptimeCheck) 
 
 	existingCheckID, err := b.findCheck(check)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to find check %s, error: %w", check.ID, err)
 	}
 	if existingCheckID == p.CheckNotFound {
 		log.FromContext(ctx).Info(fmt.Sprintf("check with ID '%s' is already deleted", check.ID))
 		return nil
 	}
 	if err = b.client.deleteMetadata(check.ID, existingCheckID); err != nil {
-		return err
+		return fmt.Errorf("failed to delete metadata for check %s (betterstack ID: %d), "+
+			"error: %w", check.ID, existingCheckID, err)
 	}
 	if err = b.client.deleteMonitor(existingCheckID); err != nil {
-		return err
+		return fmt.Errorf("failed to delete monitor for check %s (betterstack ID: %d), "+
+			"error: %w", check.ID, existingCheckID, err)
 	}
 	return nil
 }
